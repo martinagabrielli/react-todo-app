0fc18d713d168012723e5924a5d494b8fb38b491
martinagabrielli <martina.gabrielli@yahoo.it>
2020-12-09 16:06:11 +0000

    added statusHandler function and status useState

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import TodoList from './components/TodoList'
 function App() {
   const [inputText, setInputText] = useState('')
   const [todos, setTodos] = useState([])
+  const [status, setStatus] = useState('all')
+
   return (
     <div className="App">
       <header>
         <h1>My Todo List</h1>
       </header>
-      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} setStatus={setStatus} />
       <TodoList setTodos={setTodos} todos={todos} />
     </div>
   );
